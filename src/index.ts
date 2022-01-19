43647a177e3b51ec4fd9970ba56f314235b9ed51
schollsebastian <scholl.sebastian@gmx.at>
2022-01-19 15:35:41 +0100

    Create cron job for creating polls

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { Client, CommandInteraction, Intents } from 'discord.js';
+import { Client, CommandInteraction, GuildBasedChannel, Intents, MessageEmbed } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 import dotenv from 'dotenv';
-import Joke from './joke.interface';
 import { SlashCommandBuilder } from '@discordjs/builders';
+import schedule from 'node-schedule';
+import Guild from './guild.interface';
 
 dotenv.config();
 
+const cronString = '0 15 * * 5';
 const commands = [
   new SlashCommandBuilder()
     .setName('submit')
@@ -19,7 +21,9 @@ const token = process.env['NODE_ENV'] === 'development' ?
   process.env['DISCORD_PROD_TOKEN']!;
 const rest = new REST({ version: '9' }).setToken(token);
 const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] });
-const jokes: Map<string, Joke[]> = new Map<string, Joke[]>();
+const guilds: Map<string, Guild> = new Map<string, Guild>();
+
+schedule.scheduleJob(cronString, createPoll);
 
 client.on('ready', client => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -51,11 +55,20 @@ function submit(interaction: CommandInteraction) {
   const author = interaction.options.getUser('author')!;
   const joke = interaction.options.getString('joke')!;
 
-  jokes.get(interaction.guildId!)!.push({
+  guilds.get(interaction.guildId!)!.jokes.push({
     author: author.id,
     joke: joke
   });
 
   interaction.reply(`Joke submitted:\n\n${author}: "**${joke}**"`)
     .catch(console.error);
+}
+
+function createPoll() {
+  const embed = new MessageEmbed()
+    .setTitle('Vote for the Joke of the Week')
+    .setDescription('React with the emoji of the joke you think was the funniest')
+    .setColor('#boob69')
+    .setTimestamp()
+    .setThumbnail('https://cdn.discordapp.com/avatars/933319312402436206/b34986c77251abe67cf4a6909f17acc6.webp');
 }
